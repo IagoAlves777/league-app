b858b4010928090dca0fd6b3845450c5d8952f61
IagoAlves777 <iagox.alves@gmail.com>
2019-10-21 10:34:08 -0300

    Lista provisoria com os campeões

--- lib/Homepage.dart
+++ lib/Homepage.dart
@@ -1,6 +1,11 @@
 import 'package:flutter/material.dart';
 import 'package:leagueapp/ChampionAudio.dart';
 
+final listChampions = ["Aatrox","Ahri","Akali","Alistar","Amumu","Anivia","Annie","Ashe","Aurelion Sol","Azir","Bardo","Blitzcrank","Brand","Braum","Caitlyn","Camille","Cassiopeia","Cho'Gath","Corki","Darius","Diana","Dr. Mundo","Draven","Ekko","Elise","Evelynn","Ezreal","Fiddlesticks","Fiora","Fizz","Galio","Gangplank","Garen","Gnar","Gragas",
+  "Graves","Hecarim","Heimerdinger","Illaoi","Irelia","Ivern","Janna","Jarvan IV","Jax","Jayce","Jhin","Jinx","Kai'Sa","Kalista","Karma","Karthus","Kassadin","Katarina","Kayle","Kayn","Kennen","Kha'Zix","Kindred","Kled","Kog'Maw","LeBlanc","Lee Sin","Leona","Lissandra","Lucian","Lulu","Lux","Malphite","Malzahar","Maokai","Master Yi","Miss Fortune","Mordekaiser","Morgana","Nami","Nasus","Nautilus","Neeko","Nidalee",
+  "Nocturne","Nunu e Willump","Olaf","Orianna","Ornn","Pantheon","Poppy","Pyke","Qiyana","Quinn","Rakan","Rammus","Rek'Sai","Renekton","Rengar","Riven","Rumble","Ryze","Sejuani","Shaco","Shen","Shyvana","Singed","Sion","Sivir","Skarner","Sona","Soraka","Swain","Sylas","Syndra","Tahm Kench","Taliyah","Talon","Taric","Teemo","Thresh","Tristana","Trundle","Tryndamere","Twisted Fate","Twitch","Udyr","Urgot",
+  "Varus","Vayne","Veigar","Vel'Koz","Vi","Viktor","Vladimir","Volibear","Warwick","Wukong","Xayah","Xerath","Xin Zhao","Yasuo","Yorick","Yuumi","Zac","Zed","Ziggs","Zilean","Zoe","Zyra"];
+
 class HomePage extends StatefulWidget {
   @override
   _HomePageState createState() => _HomePageState();
@@ -26,7 +31,7 @@ class _HomePageState extends State<HomePage> {
           color: Colors.black87,
           child: Column(
             children: <Widget>[
-              buildTextField(),
+              //buildTextField(),
               buildListView(context),
             ],
           ),
@@ -43,53 +48,17 @@ class _HomePageState extends State<HomePage> {
 
   Widget buildListView(BuildContext context) {
     return Expanded(
-          child: ListView(
-                children: <Widget>[
-                  ListTile(
-                    title: Text('Lulu',style: TextStyle(
-                      fontSize: 26,
-                      fontFamily: 'AmaticSC Bold',
-                      fontWeight: FontWeight.bold,
-                      color: Colors.white,
-                    ),
-                    ),
-                    onTap: () => Navigator.push(
-                        context,
-                        MaterialPageRoute(builder: (context) => ChampionAudio())
-                    ),
-                  )
-                ],
-              ),
+      child: ListView.builder(
+          itemCount: listChampions.length,
+          itemBuilder: (BuildContext context, int index) {
+            return Text(listChampions[index],style: TextStyle(
+              fontSize: 30,
+              fontFamily: 'AmaticSC Bold',
+              fontWeight: FontWeight.bold,
+              color: Colors.white,
+            ),);
+          }),
     );
   }
 
-  TextField buildTextField() {
-    return Padding(
-      padding: const EdgeInsets.all(8.0),
-      child: TextField(
-                
-                style: (TextStyle(
-                  fontSize: 26,
-                  fontFamily: 'AmaticSC Bold',
-                  fontWeight: FontWeight.bold,
-                  color: Colors.white,
-                )),
-                decoration: InputDecoration(
-                    hintStyle: TextStyle(
-                      fontSize: 26,
-                      fontFamily: 'AmaticSC Bold',
-                      fontWeight: FontWeight.bold,
-                      color: Colors.white,
-                    ),
-                    hintText: 'Champions',
-                    border: InputBorder.none,
-                    enabled: true,
-                    icon: Icon(
-                      Icons.search,
-                      size: 26,
-                      color: Colors.white,
-                    )),
-              ),
-    );
-  }
 }
